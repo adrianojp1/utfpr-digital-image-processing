d671e8184b3cda97ff5c02f0861ef1d592488287
Adriano J. Paulichi <adrianopaulichi@gmail.com>
2022-10-18 22:04:05 -0300

    t4 quase la

--- t4/main.py
+++ t4/main.py
@@ -1,38 +1,17 @@
-import cv2 
-import numpy as np 
-import time
-import math
+from statistics import mean, median_low
+import cv2
+import numpy as np
 
 import sys
-sys.setrecursionlimit(1500)
+sys.setrecursionlimit(3000)
 
-ALTURA_MIN = 1
-LARGURA_MIN = 1
-N_PIXELS_MIN = 2
 
-def rotula (img, largura_min, altura_min, n_pixels_min):
-    '''Rotulagem usando flood fill. Marca os objetos da imagem com os valores
-[0.1,0.2,etc].
-
-Parâmetros: img: imagem de entrada E saída.
-            largura_min: descarta componentes com largura menor que esta.
-            altura_min: descarta componentes com altura menor que esta.
-            n_pixels_min: descarta componentes com menos pixels que isso.
-
-Valor de retorno: uma lista, onde cada item é um vetor associativo (dictionary)
-com os seguintes campos:
-
-'label': rótulo do componente.
-'n_pixels': número de pixels do componente.
-'T', 'L', 'B', 'R': coordenadas do retângulo envolvente de um componente conexo,
-respectivamente: topo, esquerda, baixo e direita.'''
+def get_blob_areas(img):
     h = img.shape[0]
     w = img.shape[1]
     label = 1.0
 
-    n_pixels_total = 0
-
-    components = []
+    areas = []
     for y in range(0, h):
         for x in range(0, w):
             if img[y, x] == 1:
@@ -46,15 +25,9 @@ respectivamente: topo, esquerda, baixo e direita.'''
                     'R': 0
                 }
                 flood_fill(img, y, x, comp)
+                areas.append(comp['n_pixels'])
+    return areas
 
-                comp_height = comp['B'] - comp['T'] + 1
-                comp_width = comp['R'] - comp['L'] + 1
-
-                if comp['n_pixels'] >= n_pixels_min and comp_width >= largura_min and comp_height >= altura_min:
-                    components.append(comp)
-                    n_pixels_total += comp['n_pixels']
-    
-    return components, n_pixels_total
 
 def flood_fill(img, y, x, comp):
     img[y][x] = comp['label']
@@ -99,52 +72,76 @@ def flood_fill(img, y, x, comp):
     if x - 1 > -1 and y - 1 > -1 and img[y - 1][x - 1] == 1:
         flood_fill(img, y, x - 1, comp)
 
-def verify_blobs_rice(components, avg_area):
-    num_rices = len(components)
 
-    for c in components:
-        if c['n_pixels'] > avg_area:
-            num_rices += math.floor(c['n_pixels']/avg_area) - 1
+def fill_borders(img):
+    im_floodfill = img.copy()
+    h, w = img.shape[:2]
+    mask = np.zeros((h + 2, w + 2), np.uint8)
+    cv2.floodFill(im_floodfill, mask, (0, 0), 255)
+    im_floodfill_inv = cv2.bitwise_not(im_floodfill)
+    return img | im_floodfill_inv
 
-    return num_rices
 
-img = cv2.imread ('82.bmp', cv2.IMREAD_GRAYSCALE)
+def estimate_rice_size(areas):
+    median = median_low(areas)
+    upper_limit = median * 1.2
+    lower_limit = median * 0.8
+    filtered = [area for area in areas if lower_limit < area < upper_limit]
+    avg = mean(filtered)
+    return avg
 
-img_out = cv2.cvtColor (img, cv2.COLOR_GRAY2BGR)
 
-img = cv2.medianBlur(img, 9)
+def count_rices(areas, median_area):
+    count = 0
 
-#TODO: Change this parameters
-img = cv2.Canny(img, 40, 140)
+    for area in areas:
+        if median_area * 0.2 < area < median_area * 1.5:
+            count += 1
 
-im_floodfill = img.copy()
+        elif area >= median_area * 1.5:
+            count += round(area / median_area)
 
-h, w = img.shape[:2]
+    return count
 
-mask = np.zeros((h + 2, w + 2), np.uint8)
 
-cv2.floodFill(im_floodfill, mask, (0, 0), 255)
+def execute(img_name):
+    print('Executing for image: ' + img_name)
+    expected_count = int(img_name.split('.')[0])
+    img = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
 
-im_floodfill_inv = cv2.bitwise_not(im_floodfill)
+    img_in = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
-im_out = img | im_floodfill_inv
+    img_blur = cv2.medianBlur(img, 7, cv2.BORDER_DEFAULT)
 
-img = im_out.astype (np.float32) / 255
+    img_border = cv2.Canny(img_blur, 50, 150, L2gradient=True)
 
-componentes, n_pixels_total = rotula (img, LARGURA_MIN, ALTURA_MIN, N_PIXELS_MIN)
+    img_filled = fill_borders(img_border)
+    img_filled = img_filled.astype(np.float32) / 255
 
-print(componentes)
-print(n_pixels_total/len(componentes))
+    areas = get_blob_areas(img_filled)
+    areas.sort()
+    # print(areas)
 
-num_rices = verify_blobs_rice(componentes, n_pixels_total/len(componentes))
+    rice_size = estimate_rice_size(areas)
+    print('Rice size: ', rice_size)
 
-for c in componentes:
-    cv2.rectangle (img_out, (c ['L'], c ['T']), (c ['R'], c ['B']), (0,0,1))
+    count = count_rices(areas, rice_size)
+    diff = abs(expected_count - count)
+    error = diff/expected_count * 100
 
-print(len(componentes))
-print(num_rices)
+    print('Expected count: ', expected_count)
+    print('Actual count: ', count)
+    print('Percentage error: ', error)
+    print('=========================================')
 
-cv2.imshow('final', img)
-cv2.imshow ('final_detec', img_out)
-cv2.waitKey ()
-cv2.destroyAllWindows ()
+    cv2.imshow('original', img_in)
+    cv2.imshow('blur', img_blur)
+    cv2.imshow('borders', img_border)
+    cv2.imshow('filled', img_filled)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
+imgs = ['60', '82', '114', '150', '205']
+for img in imgs:
+    execute(f'{img}.bmp')
